3ea203fbbcf43122df2a9450a07cfc47b22c6246
Ruth Naebeck <ruthnaebeck@gmail.com>
2017-04-12 11:55:06 -0400

    Removed home from reducers

--- app/reducers/index.jsx
+++ app/reducers/index.jsx
@@ -4,8 +4,7 @@ import campuses from './campuses';
 import campus from './campus';
 import students from './students';
 import student from './student';
-import home from './home';
 
 export default combineReducers(
-  { campuses, campus, students, student, home }
+  { campuses, campus, students, student }
 );
